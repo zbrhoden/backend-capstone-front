713826f655cee9bc2ef41a914b3ce0cd55f8373a
Blake Rhoden <zbrhoden@gmail.com>
2021-12-27 14:47:26 -0600

    Register fix

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -75,7 +75,7 @@ export const Register = () => {
                 <fieldset style={{
                     textAlign: "center"
                 }}>
-                    <button className="btn btn-1 btn-sep icon-send" type="submit">Register</button>
+                    <button className="btn btn-1 btn-sep icon-send" type="submit" onClick={handleRegister}>Register</button>
                 </fieldset>
             </form>
             <section className="link--register">
