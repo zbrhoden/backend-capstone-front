0c0b74c0c6b5aa2aedef9c1c4bc4209b38badaa2
Blake Rhoden <zbrhoden@gmail.com>
2022-01-10 15:04:52 -0600

    auth

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -3,12 +3,9 @@ import { Link, useHistory} from "react-router-dom"
 import "./Auth.css"
 
 export const Register = () => {
-    const firstName = useRef()
-    const lastName = useRef()
     const username = useRef()
     const password = useRef()
     const verifyPassword = useRef()
-    const passwordDialog = useRef()
     const history = useHistory()
 
     const handleRegister = (e) => {
@@ -17,8 +14,6 @@ export const Register = () => {
         if (password.current.value === verifyPassword.current.value) {
             const newUser = {
                 "username": username.current.value,
-                "first_name": firstName.current.value,
-                "last_name": lastName.current.value,
                 "password": password.current.value
             }
 
@@ -52,14 +47,6 @@ export const Register = () => {
 
             <form className="form--login" onSubmit={handleRegister}>
                 <h1 className="h3 mb-3 font-weight-normal">Register an account</h1>
-                <fieldset>
-                    <label htmlFor="firstName"> First Name </label>
-                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="lastName"> Last Name </label>
-                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
-                </fieldset>
                 <fieldset>
                     <label htmlFor="inputUsername">Username</label>
                     <input ref={username} type="text" name="username" className="form-control" placeholder="Username" required />
