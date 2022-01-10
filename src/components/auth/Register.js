4538c77a2b67f55115bc28e843e4639c6c4cab14
Blake Rhoden <zbrhoden@gmail.com>
2022-01-10 15:09:46 -0600

    auth

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -33,17 +33,12 @@ export const Register = () => {
                     }
                 })
         } else {
-            passwordDialog.current.showModal()
+            window.alert("Passwords do not match")
         }
     }
 
     return (
         <main style={{ textAlign: "center" }}>
-
-            <dialog className="dialog dialog--password" ref={passwordDialog}>
-                <div>Passwords do not match</div>
-                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
-            </dialog>
 
             <form className="form--login" onSubmit={handleRegister}>
                 <h1 className="h3 mb-3 font-weight-normal">Register an account</h1>
